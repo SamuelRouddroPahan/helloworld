86f16118160bdd07371a2c06bdbd369cecb21ac3
Samuel Rouddro Pahan <samuelrouddro43221@gmail.com>
2022-09-28 19:17:26 +0600

    Hello Python list

---
+++ First_and_last.py
@@ -0,0 +1,8 @@
+def printValues():
+	li = list()
+	for i in range(1,30):
+		li.append(i**2)
+	print(li[:5])
+	print(li[-5:])
+
+printValues()
---
+++ removing_even_number.py
@@ -0,0 +1,3 @@
+num = [7,8, 120, 25, 44, 20, 27]
+num2 = [x for x in num if x%2!=0]
+print("All even numbers are =",num2)
